3690dbd2982fd02b4f913600305f02d6b839050f
Landon Borrego <lndnborrrego@gmail.com>
2025-01-09 02:33:38 -0500

    Completed Extracting Dynamic Route Data

--- src/app/author/[firebaseKey]/page.js
+++ src/app/author/[firebaseKey]/page.js
@@ -1,5 +1,18 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
-export default function ViewAuthor() {
-  return <div>Here is where we will be able to view an authors details.</div>;
+export default function ViewAuthor({ params }) {
+  const { firebaseKey } = params;
+
+  return (
+    <>
+      <div>{firebaseKey} is the firebaseKey!</div>
+      <div>Here is where we will be able to view an authors details.</div>
+    </>
+  );
 }
+
+// Will change when params changes
+ViewAuthor.propTypes = {
+  params: PropTypes.string.isRequired,
+};
--- src/app/author/edit/[firebaseKey]/page.js
+++ src/app/author/edit/[firebaseKey]/page.js
@@ -1,5 +1,18 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
-export default function EditAuthor() {
-  return <div>Here is where we will be able to edit/update an author.</div>;
+export default function EditAuthor({ params }) {
+  const { firebaseKey } = params;
+
+  return (
+    <>
+      <div>{firebaseKey} is the firebaseKey!</div>
+      <div>Here is where we will be able to edit/update an author.</div>
+    </>
+  );
 }
+
+// Will change when params changes
+EditAuthor.propTypes = {
+  params: PropTypes.string.isRequired,
+};
