ce4869505d83e2db34a8617f9b6aa7967752ccce
Landon Borrego <lndnborrrego@gmail.com>
2025-01-09 01:01:34 -0500

    Completed dynamic routes

---
+++ src/app/author/[firebaseKey]/page.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ViewAuthor() {
+  return <div>Here is where we will be able to view an authors details.</div>;
+}
---
+++ src/app/author/edit/[firebaseKey]/page.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function EditAuthor() {
+  return <div>Here is where we will be able to edit/update an author.</div>;
+}
