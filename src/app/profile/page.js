6ddd58467aa950a2fcc253c3515d015663847a01
Landon Borrego <lndnborrrego@gmail.com>
2025-01-10 01:34:46 -0500

    Completed User Specific Data

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,6 +1,17 @@
+'use client';
+
 import React from 'react';
 import User from '@/components/User';
+import { Button } from 'react-bootstrap';
+import { signOut } from '@/utils/auth';
 
 export default function Profile() {
-  return <User />;
+  return (
+    <>
+      <User />
+      <Button variant="danger" onClick={signOut}>
+        Sign Out
+      </Button>
+    </>
+  );
 }
