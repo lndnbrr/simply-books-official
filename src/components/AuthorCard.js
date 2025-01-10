436d60c213976ed19d07c2b6fbcc8553e84c2b82
Landon Borrego <lndnborrrego@gmail.com>
2025-01-10 00:36:03 -0500

    Completed Working with components

---
+++ src/components/AuthorCard.js
@@ -0,0 +1,36 @@
+/* eslint-disable @next/next/no-img-element */
+
+import PropTypes from 'prop-types';
+import React from 'react';
+
+export default function AuthorCard({ authorObj }) {
+  return (
+    <div>
+      <img className="authorImage" alt="Author Headshot" {...authorObj.image} />
+      <h1 className="authorName">
+        {authorObj.first_name} {authorObj.last_name}
+      </h1>
+      <h3 className="authorEmail">{authorObj.email}</h3>
+    </div>
+  );
+}
+
+AuthorCard.propTypes = {
+  authorObj: PropTypes.shape({
+    email: PropTypes.string,
+    first_name: PropTypes.string,
+    last_name: PropTypes.bool,
+    image: PropTypes.string,
+    favorite: PropTypes.bool,
+  }),
+};
+
+AuthorCard.defaultProps = {
+  authorObj: {
+    email: 'br@gmail.com',
+    first_name: 'Brandon',
+    last_name: 'Gains',
+    image: 'https://c8.alamy.com/comp/MR0G79/random-pictures-MR0G79.jpg',
+    favorite: true,
+  },
+};
