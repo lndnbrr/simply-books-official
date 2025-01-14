4d17d4a8e94c989682d4e82894379760111c94d0
Landon Borrego <lndnborrrego@gmail.com>
2025-01-14 17:14:40 -0500

    User can now view author details.

--- src/app/author/[firebaseKey]/page.js
+++ src/app/author/[firebaseKey]/page.js
@@ -4,26 +4,27 @@ import PropTypes from 'prop-types';
 import React, { useEffect, useState } from 'react';
 import BookCard from '@/components/BookCard';
 import { Badge } from 'react-bootstrap';
-import { viewAuthorDetails } from '../../../api/mergedData';
+// import { viewAuthorDetails } from '../../../api/mergedData';
 import { getBooksByAuthor } from '../../../api/bookData';
+import { getSingleAuthor } from '../../../api/authorData';
 
 export default function ViewAuthor({ params }) {
   const { firebaseKey } = params;
 
+  // state of the viewing author details
   const [authorView, setAuthorView] = useState({});
 
+  // state of the books by a specific author
   const [authorBooks, setAuthorBooks] = useState([]);
 
-  // const getAllAuthorBooks = () => {
-  //   getSingleAuthor(firebaseKey).then(getAuthorBooks(firebaseKey)).then(setAuthorBooks)
-  // }
-
+  // hook that uses API call to get an author w/ specific fb key and sets it to be the current state of viewing author details, then uses API call to get books made by an author w/ specific fb key and sets it to be the current state of books by a specific author
   useEffect(() => {
-    viewAuthorDetails(firebaseKey).then(setAuthorView).then(getBooksByAuthor(firebaseKey).then(setAuthorBooks));
+    getSingleAuthor(firebaseKey).then(setAuthorView).then(getBooksByAuthor(firebaseKey).then(setAuthorBooks));
   }, [firebaseKey]);
 
   return (
     <>
+      {/* This is the author's details */}
       <h1>
         This is {authorView.first_name} {authorView.last_name}!
       </h1>
@@ -31,10 +32,12 @@ export default function ViewAuthor({ params }) {
       <h3>Email: {authorView.email}</h3>
       <Badge bg="danger">{authorView.favorite ? 'Favorite 🤍' : ''}</Badge>
       <hr />
+      {/* This is the books that the author made */}
       <h1>
         All books by {authorView.first_name} {authorView.last_name}!
       </h1>
       <div>
+        {/* NOTE: parentheses after arrow functioon, NOT CURLY BRACES */}
         {authorBooks.map((book) => (
           <BookCard key={book.firebaseKey} bookObj={book} />
         ))}
